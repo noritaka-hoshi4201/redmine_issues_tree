c574a4c7eca02cd0d5975bf6b1ecdbde40641b98
noritaka-hoshi4201 <noritaka-hoshi4201@genky.co.jp>
2024-03-28 14:19:56 +0900

    [update] add menu style

--- app/helpers/issues_trees_helper.rb
+++ app/helpers/issues_trees_helper.rb
@@ -1,8 +1,12 @@
 module IssuesTreesHelper
   def link_to_plain_view
-    link_to l(:back_to_plain_list, scope: 'issues_tree'),
-            {controller: :issues,
-             skip_issues_tree_redirect: true},
-            class: 'icon icon-plane-list'
+    content_tag('span', 
+              link_to( l(:back_to_plain_list, scope: 'issues_tree'),
+                    {controller: :issues,
+                    skip_issues_tree_redirect: true},
+                    class: 'icon icon-plane-list'),
+              class: 'ex-menu',
+              style: 'display:inline-block;float:left;padding-right:20px;')
+
   end
 end
--- lib/redmine_issues_tree/issues_helper_patch.rb
+++ lib/redmine_issues_tree/issues_helper_patch.rb
@@ -5,13 +5,19 @@ module RedmineIssuesTree
       base.class_eval do
         def link_to_tree_view(project = @project)
           if project.present?
-            link_to l(:tree_view, scope: 'issues_tree'), '#',
-                    class: 'icon icon-orange-tree issues-tree-view-link',
-                    data: {link_to_tree_view: redirect_with_params_project_issues_trees_path(project_id: project)}
-          else
-            link_to l(:tree_view, scope: 'issues_tree'), '#',
-                    class: 'icon icon-orange-tree issues-tree-view-link',
-                    data: {link_to_tree_view: redirect_with_params_issues_trees_path}
+            content_tag('span', 
+                        link_to( l(:tree_view, scope: 'issues_tree'), '#',
+                                class: 'icon icon-orange-tree issues-tree-view-link',
+                                data: {link_to_tree_view: redirect_with_params_project_issues_trees_path(project_id: project)}),
+                        class: 'ex-menu',
+                        style: 'display:inline-block;float:left;padding-right:20px;')
+        else
+            content_tag('span', 
+                        link_to( l(:tree_view, scope: 'issues_tree'), '#',
+                                class: 'icon icon-orange-tree issues-tree-view-link',
+                                data: {link_to_tree_view: redirect_with_params_issues_trees_path}),
+                        class: 'ex-menu',
+                        style: 'display:inline-block;float:left;padding-right:20px;')
           end
         end
       end
